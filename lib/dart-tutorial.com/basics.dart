b46bc3b8515ed610be09e069ad4d93f2fe243dc2
CodeXNilanshu <86039482+CodeXNilanshu@users.noreply.github.com>
2026-05-06 22:39:02 +0530

    dart basics

---
+++ lib/dart-tutorial.com/basics.dart
@@ -0,0 +1,27 @@
+/// write notes of programs here only in the documents comments.
+
+//1. Write a program in Dart that finds simple interest.
+// Formula= (p * t * r) / 100
+
+//code:
+// void main() {
+//   // this can be done as well
+//   // int p, q, r;
+//   int p = 10, q = 15, r = 5;
+//
+// // calculate simple interest
+//   double si = (p * q * r) / 100;
+//
+//   print("Si:$si");
+// }
+//
+
+//2. Write a program to print a square of a number using user input.
+
+//code:
+
+// import 'dart:io';
+//
+// void main() {
+//   int? s = int.trypasrse(Stdin.readLineSync());
+// }
