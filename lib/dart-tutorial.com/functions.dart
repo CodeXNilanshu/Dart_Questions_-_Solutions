55923ae7219317ffffb99ce1d98ef9c14d175494
CodeXNilanshu <86039482+CodeXNilanshu@users.noreply.github.com>
2026-05-09 23:52:59 +0530

    Functions

---
+++ lib/dart-tutorial.com/functions.dart
@@ -0,0 +1,78 @@
+//1. Write a program in Dart to print your own name using function.
+//code:
+
+//Method 1 :
+// void main() {
+//   String name = 'Nilanshu Sharma';
+//   void abc() {
+//     print(name);
+//   }
+//
+//   abc();
+// }
+
+//Method 2:
+
+// void main() {
+//   BB().abc();
+// }
+//
+// class BB {
+//   String name = 'Nilanshu Sharma';
+//   void abc() {
+//     print(name);
+//   }
+// }
+
+//----------------------------------------------------------------------------------------------------------
+
+//2. Write a program in Dart to print even numbers between intervals using function.
+//code:
+
+/**
+    When the question asks you to print even numbers "between intervals," it is asking you to:
+    Take two numbers as input (e.g., a start and an end).
+    Print all the even numbers that fall between those two values.
+
+    Example:
+    If your interval is 10 to 20:
+    Start: 10
+    End: 20
+    The "Interval" is the set of all numbers from 10, 11, 12... up to 20.
+    The Task: Print only the even ones (10, 12, 14, 16, 18, 20)
+ */
+//
+// void main() {
+//   int start = 8;
+//   int end = 35;
+//
+//   // loop
+//
+//   void fn() {
+//     for (int i = start; i <= end; i++) {
+//       if (i % 2 == 0) {
+//         print(i);
+//       }
+//     }
+//   }
+//
+//   fn();
+// }
+
+//----------------------------------------------------------------------------------------------------------
+
+//3. Create a function called greet that takes a name as an argument and prints a greeting message. For example, greet(“John”) should print “Hello, John”.
+//code:
+//
+// void main() {
+//   void greet(String s) {
+//     return print('Hello $s');
+//   }
+//
+//   greet('John');
+// }
+
+//----------------------------------------------------------------------------------------------------------
+
+//4. Write a program in Dart that generates random password.
+//code:
